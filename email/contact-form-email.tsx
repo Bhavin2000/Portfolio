4937d9d5845165a6bf96f4613846c81b5a65f7f0
Bhavin Raut <bhavinraut1@gmail.com>
2024-03-13 21:15:04 +0530

    Update contact-form-email.tsx

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -30,7 +30,7 @@ export default function ContactFormEmail({
           <Container>
             <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md ">
               <Heading className="leading-tight">
-                v
+              You received the following message from Portfolio
               </Heading>
               <Text>{message}</Text>
               <Hr />
